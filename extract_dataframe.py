09e45c29654cabdf3df5e514bda043cd9e5dfced
fredrick.chami <fredrick.chami@eganet.go.tz>
2022-08-09 20:22:51 +0300

    find statuses count

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -36,7 +36,10 @@ class TweetDfExtractor:
 
     # an example function
     def find_statuses_count(self)->list:
-        statuses_count 
+        statuses_count = []
+        count = 0
+        for element in self.tweets_list:
+            count = count +element['user']['friends_count']
         
     def find_full_text(self)->list:
         text = 
@@ -73,7 +76,7 @@ class TweetDfExtractor:
         return is_sensitive
 
     def find_favourite_count(self)->list:
-        
+        pass
     
     def find_retweet_count(self)->list:
         retweet_count = 
@@ -130,7 +133,7 @@ if __name__ == "__main__":
     # required column to be generated you should be creative and add more features
     columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
     'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
-    _, tweet_list = read_json("../covid19.json")
+    _, tweet_list = read_json("data/global_twitter_data.json")
     tweet = TweetDfExtractor(tweet_list)
     tweet_df = tweet.get_tweet_df() 
 
